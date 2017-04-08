f46b4fed94bfa942d30c7def6af4f60de384f853
TheAppFoundry <wbh26@txstate.edu>
2017-04-07 23:31:39 -0500

    Updated Splash, and Intents

--- app/src/main/java/com/bensquared/spotify/NeedLotHaveLot.java
+++ app/src/main/java/com/bensquared/spotify/NeedLotHaveLot.java
@@ -1,7 +1,11 @@
 package com.bensquared.spotify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class NeedLotHaveLot extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class NeedLotHaveLot extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_need_lot_have_lot);
 
+        TextView needASpot = (TextView)findViewById(R.id.needASpot);
+        Button haveALot = (Button)findViewById(R.id.haveALot);
+
+        haveALot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMapActivity = new Intent(getBaseContext(), MapsActivity.class);
+                startActivity(toMapActivity);
+
+            }
+        });
     }
 }
